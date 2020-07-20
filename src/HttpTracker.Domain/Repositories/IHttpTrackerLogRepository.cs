d9c84c4f40509589ba08068e896353848b5acbb7
阿星Plus <hackxing@foxmail.com>
2020-07-20 16:34:16 +0800

    🚧 仓储接口

---
+++ src/HttpTracker.Domain/Repositories/IHttpTrackerLogRepository.cs
@@ -0,0 +1,27 @@
+﻿using HttpTracker.Response;
+using System;
+using System.Threading.Tasks;
+
+namespace HttpTracker.Repositories
+{
+    public interface IHttpTrackerLogRepository
+    {
+        /// <summary>
+        /// 按条件查询HTTP请求跟踪日志数据
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="keyword"></param>
+        /// <param name="date"></param>
+        /// <param name="page"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        Task<HttpTrackerResponse<PagedList<HttpTrackerLog>>> SearchAsync(string type, string keyword, DateTime date, int page, int limit);
+
+        /// <summary>
+        /// 插入一条HTTP请求跟踪日志数据
+        /// </summary>
+        /// <param name="httpTrackerLog"></param>
+        /// <returns></returns>
+        Task<HttpTrackerResponse> InsertAsync(HttpTrackerLog httpTrackerLog);
+    }
+}
---
+++ src/HttpTracker.Domain/Repositories/IHttpTrackerLogRepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace HttpTracker.Repositories
+{
+    public interface IHttpTrackerLogRepositoryFactory
+    {
+        /// <summary>
+        /// 创建以年月分表的实例
+        /// </summary>
+        /// <param name="yearMonth"></param>
+        /// <returns></returns>
+        IHttpTrackerLogRepository CreateInstance(string yearMonth);
+    }
+}
