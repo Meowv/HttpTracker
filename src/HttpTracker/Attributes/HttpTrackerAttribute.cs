53fe5b55235ab22fd1a62dd4b7e13d6060932377
阿星Plus <hackxing@foxmail.com>
2020-07-20 15:42:00 +0800

    🆕 HttpTrackerAttribute

---
+++ src/HttpTracker/Attributes/HttpTrackerAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpTracker.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HttpTrackerAttribute : Attribute
+    {
+        public HttpTrackerAttribute()
+        {
+        }
+    }
+}
