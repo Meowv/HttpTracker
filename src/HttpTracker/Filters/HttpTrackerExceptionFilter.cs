b41a3d650502b3a4f26266ebc207e0c3db5c738e
阿星Plus <hackxing@foxmail.com>
2020-07-20 15:44:05 +0800

    🆕 HttpTrackerExceptionFilter

---
+++ src/HttpTracker/Filters/HttpTrackerExceptionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HttpTracker.Filters
+{
+    public class HttpTrackerExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception != null)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+        }
+    }
+}
