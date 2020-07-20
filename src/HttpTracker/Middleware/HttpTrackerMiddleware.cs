c01c4f64b5d926b76b8f01e11958603030c4d579
阿星Plus <hackxing@foxmail.com>
2020-07-20 15:44:19 +0800

    🆕 HttpTrackerMiddleware

---
+++ src/HttpTracker/Middleware/HttpTrackerMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace HttpTracker.Middleware
+{
+    public class HttpTrackerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HttpTrackerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
