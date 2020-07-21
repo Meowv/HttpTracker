6f6ed56643e917205fa66671fa64567920048296
阿星Plus <hackxing@foxmail.com>
2020-07-21 16:43:22 +0800

    🚧 MongoDb 测试基类

---
+++ test/HttpTracker.MongoDb.Tests/TestBase.cs
@@ -0,0 +1,25 @@
+﻿using HttpTracker.Options;
+using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpTracker.MongoDb.Tests
+{
+    public class TestBase
+    {
+        public ServiceCollection Services;
+
+        public TestBase()
+        {
+            Services = new ServiceCollection();
+
+            Services.Configure<HttpTrackerMongoDbOptions>(x =>
+            {
+                x.Services = new string[] { "127.0.0.1:27017" };
+                x.DatabaseName = "meowv";
+            });
+
+            Services.AddSingleton<IMongoDbProvider, MongoDbProvider>();
+            Services.AddSingleton<IHttpTrackerLogRepositoryFactory, HttpTrackerLogRepositoryFactory>();
+        }
+    }
+}
