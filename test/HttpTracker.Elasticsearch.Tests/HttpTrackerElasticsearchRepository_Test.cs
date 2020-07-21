d3822c06bb3a8667c6c3c95b52925075f0c3bc66
阿星Plus <hackxing@foxmail.com>
2020-07-21 11:25:28 +0800

    ✅ Elasticsearch 创建仓储实例测试

---
+++ test/HttpTracker.Elasticsearch.Tests/HttpTrackerElasticsearchRepository_Test.cs
@@ -0,0 +1,22 @@
+﻿using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.Elasticsearch.Tests
+{
+    public class HttpTrackerElasticsearchRepository_Test : TestBase
+    {
+        [Fact]
+        public void CreateInstance()
+        {
+            Services.AddSingleton<IElasticsearchProvider, ElasticsearchProvider>();
+            Services.AddSingleton<IHttpTrackerLogRepositoryFactory, HttpTrackerLogRepositoryFactory>();
+
+            var factory = Services.BuildServiceProvider().GetRequiredService<IHttpTrackerLogRepositoryFactory>();
+
+            var repository = factory.CreateInstance(yearMonth);
+
+            Assert.NotNull(repository);
+        }
+    }
+}
