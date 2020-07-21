ad610d30c8b9086d0144269fdc6a336d5adc3ab5
阿星Plus <hackxing@foxmail.com>
2020-07-21 11:25:49 +0800

    ✅ Elasticsearch 实例获取测试

--- test/HttpTracker.Elasticsearch.Tests/HttpTrackerElasticsearchProvider_Test.cs
+++ test/HttpTracker.Elasticsearch.Tests/HttpTrackerElasticsearchProvider_Test.cs
@@ -1,24 +1,16 @@
-﻿using HttpTracker.Options;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace HttpTracker.Elasticsearch.Tests
 {
-    public class HttpTrackerElasticsearchProvider_Test
+    public class HttpTrackerElasticsearchProvider_Test : TestBase
     {
         [Fact]
         public void GetClient()
         {
-            var services = new ServiceCollection();
+            Services.AddTransient<IElasticsearchProvider, ElasticsearchProvider>();
 
-            services.Configure<HttpTrackerElasticsearchOptions>(x =>
-            {
-                x.Nodes = new string[] { "http://127.0.0.1:9200" };
-            });
-
-            services.AddTransient<IElasticsearchProvider, ElasticsearchProvider>();
-
-            var provider = services.BuildServiceProvider().GetRequiredService<IElasticsearchProvider>();
+            var provider = Services.BuildServiceProvider().GetRequiredService<IElasticsearchProvider>();
             var client = provider.GetClient();
 
             Assert.NotNull(client);
