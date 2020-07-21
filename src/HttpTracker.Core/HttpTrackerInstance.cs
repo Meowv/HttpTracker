89614222281571c9284bc85ec4fc171a80d101c2
阿星Plus <hackxing@foxmail.com>
2020-07-21 22:55:51 +0800

    ⚡ HttpTracker InstanceName

--- src/HttpTracker.Core/HttpTrackerInstance.cs
+++ src/HttpTracker.Core/HttpTrackerInstance.cs
@@ -4,6 +4,6 @@ namespace HttpTracker
 {
     public static class HttpTrackerInstance
     {
-        public static string InstanceName => DateTimeOffset.UtcNow.LocalDateTime.ToString("yyyyMM");
+        public static string InstanceName => DateTime.Now.ToString("yyyyMM");
     }
 }
