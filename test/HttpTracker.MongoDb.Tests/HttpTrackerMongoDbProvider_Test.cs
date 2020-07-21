fa11b9f8e8522c9ab7b089eb88c876ff505c5392
阿星Plus <hackxing@foxmail.com>
2020-07-21 16:44:08 +0800

    ✅ MongoDb 获取数据库实例测试

---
+++ test/HttpTracker.MongoDb.Tests/HttpTrackerMongoDbProvider_Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.MongoDb.Tests
+{
+    public class HttpTrackerMongoDbProvider_Test : TestBase
+    {
+        [Fact]
+        public void GetClient()
+        {
+            var provider = Services.BuildServiceProvider().GetRequiredService<IMongoDbProvider>();
+
+            var client = provider.Client;
+            var database = provider.Database;
+
+            Assert.NotNull(client);
+            Assert.NotNull(database);
+        }
+    }
+}
