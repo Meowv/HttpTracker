5920074a82dc45ba9fdc4da90e5534d159a78d7f
阿星Plus <hackxing@foxmail.com>
2020-07-22 12:41:33 +0800

    ✅ MySql 测试基类

---
+++ test/HttpTracker.MySql.Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using HttpTracker.Options;
+using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpTracker.MySql.Tests
+{
+    public class TestBase
+    {
+        public ServiceCollection Services;
+
+        public TestBase()
+        {
+            Services = new ServiceCollection();
+
+            Services.Configure<HttpTrackerMySqlOptions>(x =>
+            {
+                x.ConnectionString = "Server=127.0.0.1;User Id=root;Password=123456;Database=meowv;";
+            });
+
+            Services.AddSingleton<IDbConnectionProvider, MySqlProvider>();
+            Services.AddSingleton<IHttpTrackerLogRepositoryFactory, HttpTrackerLogRepositoryFactory>();
+        }
+    }
+}
