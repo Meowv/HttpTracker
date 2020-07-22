97bc0b8ddcc893d6d3b7c186ac5aace3f5f92bf9
阿星Plus <hackxing@foxmail.com>
2020-07-22 16:37:09 +0800

    🆕 HttpTracker 配置项字段更新

--- src/HttpTracker/Options/HttpTrackerOptions.cs
+++ src/HttpTracker/Options/HttpTrackerOptions.cs
@@ -7,17 +7,27 @@ namespace HttpTracker.Options
         /// <summary>
         /// 是否禁用
         /// </summary>
-        public bool Disabled { get; set; }
+        public bool Disabled { get; set; } = false;
 
         /// <summary>
-        /// 监听服务名称
+        /// 服务名称
         /// </summary>
-        public string Name { get; set; }
+        public string ServerName { get; set; } = "api_meowv";
+
+        /// <summary>
+        /// 服务主机
+        /// </summary>
+        public string ServerHost { get; set; } = "132.232.126.92";
+
+        /// <summary>
+        /// 服务端口
+        /// </summary>
+        public int ServerPort { get; set; } = 5000;
 
         /// <summary>
         /// 过滤请求
         /// </summary>
-        public string[] FilterRequest { get; set; }
+        public string[] FilterRequest { get; set; } = { "/swagger" };
 
         public HttpTrackerOptions Value => this;
     }
