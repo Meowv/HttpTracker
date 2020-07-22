1f096dfbb488fe9debe7fa8734e585b43c338f33
阿星Plus <hackxing@foxmail.com>
2020-07-22 12:46:43 +0800

    ✅ MySql 创建实例测试

---
+++ test/HttpTracker.MySql.Tests/HttpTrackerMySqlRepository_Test.cs
@@ -0,0 +1,18 @@
+﻿using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.MySql.Tests
+{
+    public class HttpTrackerMySqlRepository_Test : TestBase
+    {
+        [Fact]
+        public void CreateInstance()
+        {
+            var factory = Services.BuildServiceProvider().GetRequiredService<IHttpTrackerLogRepositoryFactory>();
+            var repository = factory.CreateInstance(HttpTrackerInstance.InstanceName);
+
+            Assert.NotNull(repository);
+        }
+    }
+}
