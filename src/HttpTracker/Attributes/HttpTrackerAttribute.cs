d9f4fcae8e8d084e429201247a5da0ff4f45672b
阿星Plus <hackxing@foxmail.com>
2020-07-22 14:41:09 +0800

    🆕 HttpTracker Attribute

--- src/HttpTracker/Attributes/HttpTrackerAttribute.cs
+++ src/HttpTracker/Attributes/HttpTrackerAttribute.cs
@@ -2,11 +2,36 @@
 
 namespace HttpTracker.Attributes
 {
+    /// <summary>
+    /// HttpTracker Attribute
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class HttpTrackerAttribute : Attribute
     {
-        public HttpTrackerAttribute()
+        public HttpTrackerAttribute(string description)
         {
+            Description = description;
         }
+
+        public HttpTrackerAttribute(bool disabled)
+        {
+            Disabled = disabled;
+        }
+
+        public HttpTrackerAttribute(string description, bool disabled)
+        {
+            Disabled = disabled;
+            Description = description;
+        }
+
+        /// <summary>
+        /// 是否禁用
+        /// </summary>
+        public bool Disabled { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
     }
 }
