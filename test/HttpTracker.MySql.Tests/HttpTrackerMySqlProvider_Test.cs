c597b2bc247ddcf9ceb1c7974803c514d20ee8a2
阿星Plus <hackxing@foxmail.com>
2020-07-22 12:42:00 +0800

    ✅ 获取 MySql 连接字符串测试

---
+++ test/HttpTracker.MySql.Tests/HttpTrackerMySqlProvider_Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.MySql.Tests
+{
+    public class HttpTrackerMySqlProvider_Test : TestBase
+    {
+        [Fact]
+        public void GetConnection()
+        {
+            var provider = Services.BuildServiceProvider().GetRequiredService<IDbConnectionProvider>();
+
+            var connection = provider.Connection;
+
+            Assert.NotNull(connection);
+        }
+    }
+}
