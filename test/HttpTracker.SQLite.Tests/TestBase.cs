add7ef1727f7912f6bb1ecbd4c6f06da711b928f
阿星Plus <hackxing@foxmail.com>
2020-07-23 15:05:51 +0800

    🆕 SQLite 测试基类

---
+++ test/HttpTracker.SQLite.Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using HttpTracker.Options;
+using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpTracker.SQLite.Tests
+{
+    public class TestBase
+    {
+        public ServiceCollection Services;
+
+        public TestBase()
+        {
+            Services = new ServiceCollection();
+
+            Services.Configure<HttpTrackerSQLiteOptions>(x =>
+            {
+                x.ConnectionString = "Data Source=D:/meowv.db;";
+            });
+
+            Services.AddSingleton<IDbConnectionProvider, SQLiteProvider>();
+            Services.AddSingleton<IHttpTrackerLogRepositoryFactory, HttpTrackerLogRepositoryFactory>();
+        }
+    }
+}
