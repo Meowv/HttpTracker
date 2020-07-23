a8ca892347099f6a4f453418cf22eae34a099f84
阿星Plus <hackxing@foxmail.com>
2020-07-23 11:51:00 +0800

    🆕 SQLServer 初始化数据库表、添加和查询数据完成

--- src/HttpTracker.SQLServer/Repositories/HttpTrackerLogRepository.cs
+++ src/HttpTracker.SQLServer/Repositories/HttpTrackerLogRepository.cs
@@ -1,9 +1,12 @@
-﻿using HttpTracker.Domain;
+﻿using Dapper;
+using HttpTracker.Domain;
 using HttpTracker.Dto;
 using HttpTracker.Dto.Params;
 using HttpTracker.Options;
 using HttpTracker.Response;
 using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpTracker.Repositories
@@ -23,19 +26,156 @@ namespace HttpTracker.Repositories
 
         protected override string TableName { get; }
 
-        public Task<HttpTrackerResponse> InitAsync()
+        public async Task<HttpTrackerResponse> InitAsync()
         {
-            throw new NotImplementedException();
+            var response = new HttpTrackerResponse();
+
+            try
+            {
+                using (Connection)
+                {
+                    var sql = $@"IF NOT EXISTS (SELECT * from sysobjects where id = object_id('{TableName}'))
+                                 BEGIN
+                                     CREATE TABLE [dbo].[{TableName}](
+	                                     [Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	                                     [Type] [nvarchar](20) NOT NULL,
+	                                     [Description] [nvarchar](200) NULL,
+	                                     [UserAgent] [nvarchar](200) NULL,
+	                                     [Method] [nvarchar](20) NOT NULL,
+	                                     [Url] [nvarchar](200) NOT NULL,
+	                                     [Referrer] [nvarchar](200) NULL,
+	                                     [IpAddress] [nvarchar](20) NOT NULL,
+	                                     [Milliseconds] [int] NOT NULL,
+	                                     [QueryString] [nvarchar](200) NULL,
+	                                     [RequestBody] [nvarchar](max) NULL,
+	                                     [Cookies] [nvarchar](max) NULL,
+	                                     [Headers] [nvarchar](max) NULL,
+	                                     [StatusCode] [int] NOT NULL,
+	                                     [ResponseBody] [nvarchar](max) NULL,
+	                                     [ServerName] [nvarchar](50) NULL,
+	                                     [PId] [int] NULL,
+	                                     [Host] [nvarchar](20) NULL,
+	                                     [Port] [int] NULL,
+	                                     [ExceptionType] [nvarchar](50) NULL,
+	                                     [Message] [nvarchar](200) NULL,
+	                                     [StackTrace] [nvarchar](max) NULL,
+	                                     [CreationTime] [datetime] NOT NULL
+                                     )
+                                 END";
+
+                    await Connection.ExecuteAsync(sql);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsFailed(ex);
+            }
+
+            return response;
         }
 
-        public Task<HttpTrackerResponse> InsertAsync(HttpTrackerLog httpTrackerLog)
+        public async Task<HttpTrackerResponse> InsertAsync(HttpTrackerLog httpTrackerLog)
         {
-            throw new NotImplementedException();
+            var response = new HttpTrackerResponse();
+
+            if (httpTrackerLog == null)
+            {
+                response.IsFailed("HttpTrackerLog is null");
+                return response;
+            }
+
+            try
+            {
+                var sql = $@"INSERT INTO [dbo].[{TableName}] ([Type] ,[Description] ,[UserAgent] ,[Method] ,[Url] ,[Referrer] ,[IpAddress] ,[Milliseconds] ,[QueryString] ,[RequestBody] ,[Cookies] ,[Headers] ,[StatusCode] ,[ResponseBody] ,[ServerName] ,[PId] ,[Host] ,[Port] ,[ExceptionType] ,[Message] ,[StackTrace] ,[CreationTime]) VALUES (@Type, @Description, @UserAgent, @Method, @Url, @Referrer, @IpAddress, @Milliseconds, @QueryString, @RequestBody, @Cookies, @Headers, @StatusCode, @ResponseBody, @ServerName, @PId, @Host, @Port, @ExceptionType, @Message, @StackTrace, @CreationTime);";
+
+                using (Connection)
+                {
+                    await Connection.ExecuteAsync(sql, httpTrackerLog);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsFailed(ex);
+            }
+
+            return response;
         }
 
-        public Task<HttpTrackerResponse<PagedList<HttpTrackerLogDto>>> QueryAsync(QueryInput input)
+        public async Task<HttpTrackerResponse<PagedList<HttpTrackerLogDto>>> QueryAsync(QueryInput input)
         {
-            throw new NotImplementedException();
+            var response = new HttpTrackerResponse<PagedList<HttpTrackerLogDto>>();
+
+            var builder = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(input.Type))
+            {
+                builder.Append($" AND Type LIKE @Type");
+
+                input.Type = $"%{input.Type}%";
+            }
+            if (!string.IsNullOrEmpty(input.Keyword))
+            {
+                builder.Append($" AND Description LIKE @Keyword");
+
+                input.Keyword = $"%{input.Keyword}%";
+            }
+
+            var where = builder.ToString();
+
+            var sql = $@"SELECT COUNT(1) FROM {TableName} WHERE 1 = 1 {where};
+                         SELECT Type, Description, UserAgent, Method, Url , Referrer, IpAddress, Milliseconds, QueryString, RequestBody , Cookies, Headers, StatusCode, ResponseBody, ServerName , PId, Host, Port, ExceptionType, Message , StackTrace, CreationTime FROM (SELECT ROW_NUMBER() OVER(ORDER BY CreationTime DESC) AS Number, * FROM {TableName} WHERE 1= 1 {where} ) AS t WHERE t.Number BETWEEN @page AND @limit";
+            
+            using (Connection)
+            {
+                input.Page = (input.Page - 1) * (input.Limit + 1);
+                input.Limit = input.Page * input.Limit;
+
+                var query = await Connection.QueryMultipleAsync(sql, input);
+
+                var total = await query.ReadFirstOrDefaultAsync<long>();
+                var logs = await query.ReadAsync<HttpTrackerLog>();
+
+                var list = logs.Select(x => new HttpTrackerLogDto
+                {
+                    Type = x.Type,
+                    Description = x.Description,
+                    Request = new RequestInfo
+                    {
+                        UserAgent = x.UserAgent,
+                        Method = x.Method,
+                        Url = x.Url,
+                        Referrer = x.Referrer,
+                        IpAddress = x.IpAddress,
+                        Milliseconds = x.Milliseconds,
+                        RequestBody = x.RequestBody,
+                        Cookies = x.Cookies,
+                        Headers = x.Headers
+                    },
+                    Response = new ResponseInfo
+                    {
+                        StatusCode = x.StatusCode,
+                        ResponseBody = x.ResponseBody
+                    },
+                    Server = new ServerInfo
+                    {
+                        ServerName = x.ServerName,
+                        PId = x.PId,
+                        Host = x.Host,
+                        Port = x.Port
+                    },
+                    Exception = new ExceptionInfo
+                    {
+                        ExceptionType = x.ExceptionType,
+                        Message = x.Message,
+                        StackTrace = x.StackTrace
+                    },
+                    CreationTime = x.CreationTime
+                }).ToList();
+
+                response.IsSuccess(new PagedList<HttpTrackerLogDto>(Convert.ToInt32(total), list));
+            }
+
+            return response;
         }
     }
 }
