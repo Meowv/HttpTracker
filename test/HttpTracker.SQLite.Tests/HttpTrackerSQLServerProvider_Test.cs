f9004aac538a3f28d12cd35e9ff6a5d1ec2e88a0
阿星Plus <hackxing@foxmail.com>
2020-07-23 15:06:12 +0800

    🆕 SQLite 连接测试

---
+++ test/HttpTracker.SQLite.Tests/HttpTrackerSQLServerProvider_Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.SQLite.Tests
+{
+    public class HttpTrackerSQLiteProvider_Test : TestBase
+    {
+        [Fact]
+        public void GetConnection()
+        {
+            var provider = Services.BuildServiceProvider().GetRequiredService<IDbConnectionProvider>();
+
+            var connection = provider.Connection;
+
+            Assert.NotNull(connection);
+        }
+    }
+}
