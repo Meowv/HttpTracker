d97a3a13a6bcacd9b7f35dd590adac2250c4c5e6
阿星Plus <hackxing@foxmail.com>
2020-07-23 13:20:10 +0800

    🆕 SQLServer 测试基类

---
+++ test/HttpTracker.SQLServer.Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using HttpTracker.Options;
+using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpTracker.SQLServer.Tests
+{
+    public class TestBase
+    {
+        public ServiceCollection Services;
+
+        public TestBase()
+        {
+            Services = new ServiceCollection();
+
+            Services.Configure<HttpTrackerSQLServerOptions>(x =>
+            {
+                x.ConnectionString = "Data Source=.;Initial Catalog=meowv;Persist Security Info=True;User ID=sa;Password=123456";
+            });
+
+            Services.AddSingleton<IDbConnectionProvider, SQLServerProvider>();
+            Services.AddSingleton<IHttpTrackerLogRepositoryFactory, HttpTrackerLogRepositoryFactory>();
+        }
+    }
+}
