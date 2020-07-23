7887043f4f7fcd4c81cdff1924cc3775ea045520
阿星Plus <hackxing@foxmail.com>
2020-07-23 13:22:16 +0800

    ✅ SQLServer 创建实例测试

---
+++ test/HttpTracker.SQLServer.Tests/HttpTrackerSQLServerRepository_Test.cs
@@ -0,0 +1,18 @@
+﻿using HttpTracker.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.SQLServer.Tests
+{
+    public class HttpTrackerSQLServerRepository_Test : TestBase
+    {
+        [Fact]
+        public void CreateInstance()
+        {
+            var factory = Services.BuildServiceProvider().GetRequiredService<IHttpTrackerLogRepositoryFactory>();
+            var repository = factory.CreateInstance(HttpTrackerInstance.InstanceName);
+
+            Assert.NotNull(repository);
+        }
+    }
+}
