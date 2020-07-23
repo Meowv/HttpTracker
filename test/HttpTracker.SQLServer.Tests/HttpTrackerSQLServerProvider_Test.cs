c8401f2b71c3da5f53ee671331d3c953979b1f56
阿星Plus <hackxing@foxmail.com>
2020-07-23 13:20:38 +0800

    🆕 SQLServer 连接测试

---
+++ test/HttpTracker.SQLServer.Tests/HttpTrackerSQLServerProvider_Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpTracker.SQLServer.Tests
+{
+    public class HttpTrackerSQLServerProvider_Test : TestBase
+    {
+        [Fact]
+        public void GetConnection()
+        {
+            var provider = Services.BuildServiceProvider().GetRequiredService<IDbConnectionProvider>();
+
+            var connection = provider.Connection;
+
+            Assert.NotNull(connection);
+        }
+    }
+}
