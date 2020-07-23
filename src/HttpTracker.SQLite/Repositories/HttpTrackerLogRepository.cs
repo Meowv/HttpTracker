79a1eeb33dd7f28aa6abf465a05af42a3c3e88f7
阿星Plus <hackxing@foxmail.com>
2020-07-23 14:53:06 +0800

    🆕 SQLite 仓储实现，初始化数据库表、新增数据、分页查询数据完成

--- src/HttpTracker.SQLite/Repositories/HttpTrackerLogRepository.cs
+++ src/HttpTracker.SQLite/Repositories/HttpTrackerLogRepository.cs
@@ -1,9 +1,12 @@
-﻿using HttpTracker.Domain;
+﻿using Dapper;
+using HttpTracker.Domain;
 using HttpTracker.Dto;
 using HttpTracker.Dto.Params;
 using HttpTracker.Options;
 using HttpTracker.Response;
 using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpTracker.Repositories
@@ -23,19 +26,152 @@ namespace HttpTracker.Repositories
 
         protected override string TableName { get; }
 
-        public Task<HttpTrackerResponse> InitAsync()
+        public async Task<HttpTrackerResponse> InitAsync()
         {
-            throw new NotImplementedException();
+            var response = new HttpTrackerResponse();
+
+            try
+            {
+                using (Connection)
+                {
+                    var sql = $@"create table if not exists {TableName}(
+                                    Id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                    Type text(20) NOT NULL,
+                                    Description text(200),
+                                    UserAgent text(200),
+                                    Method text(20) NOT NULL,
+                                    Url text(200) NOT NULL,
+                                    Referrer text(200),
+                                    IpAddress text(20) NOT NULL,
+                                    Milliseconds integer NOT NULL,
+                                    QueryString text(200),
+                                    RequestBody text,
+                                    Cookies text,
+                                    Headers text,
+                                    StatusCode integer NOT NULL,
+                                    ResponseBody text,
+                                    ServerName text(50),
+                                    PId integer,
+                                    Host text(20),
+                                    Port integer,
+                                    ExceptionType text(50),
+                                    Message text(200),
+                                    StackTrace text,
+                                    CreationTime text(7) NOT NULL
+                                 )";
+
+                    await Connection.ExecuteAsync(sql);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsFailed(ex);
+            }
+
+            return response;
         }
 
-        public Task<HttpTrackerResponse> InsertAsync(HttpTrackerLog httpTrackerLog)
+        public async Task<HttpTrackerResponse> InsertAsync(HttpTrackerLog httpTrackerLog)
         {
-            throw new NotImplementedException();
+            var response = new HttpTrackerResponse();
+
+            if (httpTrackerLog == null)
+            {
+                response.IsFailed("HttpTrackerLog is null");
+                return response;
+            }
+
+            try
+            {
+                var sql = $@"INSERT INTO {TableName}(Type ,Description ,UserAgent ,Method ,Url ,Referrer ,IpAddress ,Milliseconds ,QueryString ,RequestBody ,Cookies ,Headers ,StatusCode ,ResponseBody ,ServerName ,PId ,Host ,Port ,ExceptionType ,Message ,StackTrace ,CreationTime) VALUES (@Type, @Description, @UserAgent, @Method, @Url, @Referrer, @IpAddress, @Milliseconds, @QueryString, @RequestBody, @Cookies, @Headers, @StatusCode, @ResponseBody, @ServerName, @PId, @Host, @Port, @ExceptionType, @Message, @StackTrace, @CreationTime);";
+
+                using (Connection)
+                {
+                    await Connection.ExecuteAsync(sql, httpTrackerLog);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsFailed(ex);
+            }
+
+            return response;
         }
 
-        public Task<HttpTrackerResponse<PagedList<HttpTrackerLogDto>>> QueryAsync(QueryInput input)
+        public async Task<HttpTrackerResponse<PagedList<HttpTrackerLogDto>>> QueryAsync(QueryInput input)
         {
-            throw new NotImplementedException();
+            var response = new HttpTrackerResponse<PagedList<HttpTrackerLogDto>>();
+
+            var builder = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(input.Type))
+            {
+                builder.Append($" AND Type LIKE @Type");
+
+                input.Type = $"%{input.Type}%";
+            }
+            if (!string.IsNullOrEmpty(input.Keyword))
+            {
+                builder.Append($" AND Description LIKE @Keyword");
+
+                input.Keyword = $"%{input.Keyword}%";
+            }
+
+            var where = builder.ToString();
+
+            var sql = $@"SELECT COUNT(1) FROM {TableName} WHERE 1 = 1 {where};
+                         SELECT Type, Description, UserAgent, Method, Url , Referrer, IpAddress, Milliseconds, QueryString, RequestBody , Cookies, Headers, StatusCode, ResponseBody, ServerName , PId, Host, Port, ExceptionType, Message , StackTrace, CreationTime FROM {TableName} WHERE 1 = 1 ORDER BY CreationTime DESC LIMIT @page,@limit";
+
+            using (Connection)
+            {
+                input.Page = (input.Page - 1) * input.Limit;
+
+                var query = await Connection.QueryMultipleAsync(sql, input);
+
+                var total = await query.ReadFirstOrDefaultAsync<int>();
+                var logs = await query.ReadAsync<HttpTrackerLog>();
+
+                var list = logs.Select(x => new HttpTrackerLogDto
+                {
+                    Type = x.Type,
+                    Description = x.Description,
+                    Request = new RequestInfo
+                    {
+                        UserAgent = x.UserAgent,
+                        Method = x.Method,
+                        Url = x.Url,
+                        Referrer = x.Referrer,
+                        IpAddress = x.IpAddress,
+                        Milliseconds = x.Milliseconds,
+                        RequestBody = x.RequestBody,
+                        Cookies = x.Cookies,
+                        Headers = x.Headers
+                    },
+                    Response = new ResponseInfo
+                    {
+                        StatusCode = x.StatusCode,
+                        ResponseBody = x.ResponseBody
+                    },
+                    Server = new ServerInfo
+                    {
+                        ServerName = x.ServerName,
+                        PId = x.PId,
+                        Host = x.Host,
+                        Port = x.Port
+                    },
+                    Exception = new ExceptionInfo
+                    {
+                        ExceptionType = x.ExceptionType,
+                        Message = x.Message,
+                        StackTrace = x.StackTrace
+                    },
+                    CreationTime = x.CreationTime
+                }).ToList();
+
+                response.IsSuccess(new PagedList<HttpTrackerLogDto>(total, list));
+            }
+
+            return response;
         }
     }
 }
