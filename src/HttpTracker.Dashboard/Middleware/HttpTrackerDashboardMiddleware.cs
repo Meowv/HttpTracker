80c3195080de7a0d838606860c8fca783650b3bf
阿星Plus <hackxing@foxmail.com>
2020-07-24 21:41:59 +0800

    ⚡ HttpTracker.Dashboard 中间件优化

--- src/HttpTracker.Dashboard/Middleware/HttpTrackerDashboardMiddleware.cs
+++ src/HttpTracker.Dashboard/Middleware/HttpTrackerDashboardMiddleware.cs
@@ -1,21 +1,20 @@
 ﻿using HttpTracker.Options;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-#if NETSTANDARD2_1
-
-using Microsoft.AspNetCore.Hosting;
-
-#elif NETCOREAPP3_1
-
+#if NETCOREAPP3_1
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IWebHostEnvironment;
-
 #endif
 
 namespace HttpTracker.Middleware
@@ -36,17 +35,21 @@ namespace HttpTracker.Middleware
 
         public async Task Invoke(HttpContext httpContext)
         {
-            if (string.Equals(httpContext.Request.Method, "GET") && httpContext.Request.Path.StartsWithSegments(_options.RoutePrefix))
+            var httpMethod = httpContext.Request.Method;
+            var path = httpContext.Request.Path.Value;
+
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"^/?{Regex.Escape(_options.RoutePrefix)}/?$"))
             {
-                var path = httpContext.Request.Path.Value;
+                var relativeRedirectPath = path.EndsWith("/") ? "index.html" : $"{path.Split('/').Last()}/index.html";
 
-                if (path.Equals(_options.RoutePrefix, StringComparison.OrdinalIgnoreCase) || (path[^1] == '/' && path.Length == _options.RoutePrefix.Length + 1))
-                {
-                    httpContext.Response.StatusCode = 302;
-                    httpContext.Response.Headers["Location"] = $"{_options.RoutePrefix}/index.html";
+                RespondWithRedirect(httpContext.Response, relativeRedirectPath);
+                return;
+            }
 
-                    return;
-                }
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"^{Regex.Escape(_options.RoutePrefix)}/index.html$"))
+            {
+                await RespondWithIndexHtml(httpContext.Response);
+                return;
             }
 
             await _staticFileMiddleware.Invoke(httpContext);
@@ -56,11 +59,41 @@ namespace HttpTracker.Middleware
         {
             var staticFileOptions = new StaticFileOptions
             {
-                RequestPath = string.IsNullOrEmpty(_options.RoutePrefix) ? string.Empty : _options.RoutePrefix,
+                RequestPath = string.IsNullOrEmpty(_options.RoutePrefix) ? string.Empty : $"/{_options.RoutePrefix}",
                 FileProvider = new EmbeddedFileProvider(typeof(HttpTrackerDashboardMiddleware).Assembly, EmbeddedFileNamespace),
             };
 
             return new StaticFileMiddleware(next, hostingEnv, Microsoft.Extensions.Options.Options.Create(staticFileOptions), loggerFactory);
+        }
+
+        private void RespondWithRedirect(HttpResponse response, string location)
+        {
+            response.StatusCode = 301;
+            response.Headers["Location"] = location;
+        }
+
+        private async Task RespondWithIndexHtml(HttpResponse response)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/html;charset=utf-8";
+
+            using var stream = _options.IndexStream();
+
+            var htmlBuilder = new StringBuilder(new StreamReader(stream).ReadToEnd());
+            foreach (var entry in GetIndexArguments())
+            {
+                htmlBuilder.Replace(entry.Key, entry.Value);
+            }
+
+            await response.WriteAsync(htmlBuilder.ToString(), Encoding.UTF8);
+        }
+
+        private IDictionary<string, string> GetIndexArguments()
+        {
+            return new Dictionary<string, string>
+            {
+                { "%(DocumentTitle)", _options.DocumentTitle },
+            };
         }
     }
 }
