55761117ebd192679760dd3589bd6b7e7388099e
阿星Plus <hackxing@foxmail.com>
2020-07-24 12:39:08 +0800

    🆕 HttpTrackerDashboardOptions

---
+++ src/HttpTracker.Dashboard/Options/HttpTrackerDashboardOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace HttpTracker.Options
+{
+    public class HttpTrackerDashboardOptions : IOptions<HttpTrackerDashboardOptions>
+    {
+        public string RoutePrefix { get; set; } = "httptracker";
+
+        public string DocumentTitle { get; set; } = "HttpTracker Dashboard";
+
+        public string Username { get; set; } = "meowv";
+
+        public string Password { get; set; } = "123456";
+
+        public HttpTrackerDashboardOptions Value => this;
+    }
+}
