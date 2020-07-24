a0fe7ec4de5723c4430ea989257660cf1f0f8481
阿星Plus <hackxing@foxmail.com>
2020-07-24 12:39:53 +0800

    🆕 HttpTrackerDashboardExtensions 扩展方法

---
+++ src/HttpTracker.Dashboard/Extensions/HttpTrackerDashboardExtensions.cs
@@ -0,0 +1,35 @@
+﻿using HttpTracker.Options;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HttpTracker.Extensions
+{
+    public static class HttpTrackerDashboardExtensions
+    {
+        public static IHttpTrackerBuilder UseHttpTrackerDashboard(this IHttpTrackerBuilder builder)
+        {
+            builder.Services.AddOptions();
+            builder.Services.Configure<HttpTrackerDashboardOptions>(builder.Configuration.GetSection("Storage:Dashboard"));
+
+            return builder.UseHttpTrackerDashboardService();
+        }
+
+        public static IHttpTrackerBuilder UseHttpTrackerDashboard(this IHttpTrackerBuilder builder, Action<HttpTrackerDashboardOptions> options)
+        {
+            builder.Services.AddOptions();
+            builder.Services.Configure(options);
+
+            return builder.UseHttpTrackerDashboardService();
+        }
+
+        internal static IHttpTrackerBuilder UseHttpTrackerDashboardService(this IHttpTrackerBuilder builder)
+        {
+            builder.Services.AddMvcCore(options =>
+            {
+                options.Conventions.Add(new HttpTrackerApplicationModelConvention());
+            }).PartManager.FeatureProviders.Add(new HttpTrackerControllerFeatureProvider());
+
+            return builder;
+        }
+    }
+}
