56f033553a0265d87050015a6f1213ad81010d36
阿星Plus <hackxing@foxmail.com>
2020-07-24 12:37:52 +0800

    🚧 自定义控制器发现

---
+++ src/HttpTracker.Dashboard.API/HttpTrackerApplicationModelConvention.cs
@@ -0,0 +1,36 @@
+﻿using HttpTracker.Controller;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Linq;
+
+namespace HttpTracker
+{
+    public class HttpTrackerApplicationModelConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            foreach (var controller in application.Controllers)
+            {
+                if (controller.ControllerType == typeof(HttpTrackerController))
+                {
+                    controller.ApiExplorer.GroupName = "HttpTrackerDashboardAPI";
+                    controller.ApiExplorer.IsVisible = true;
+
+                    var a = controller.Attributes;
+
+                    foreach (var action in controller.Actions)
+                    {
+                        var route = (action.Attributes.FirstOrDefault(x => x.GetType() == typeof(RouteAttribute)) as RouteAttribute)?.Template;
+                        if (string.IsNullOrEmpty(route))
+                        {
+                            route = action.ActionName;
+                        }
+
+                        action.ApiExplorer.IsVisible = true;
+                        action.Selectors.First().AttributeRouteModel = new AttributeRouteModel(new RouteAttribute($"httptracker/api/{route}"));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/HttpTracker.Dashboard.API/HttpTrackerControllerFeatureProvider.cs
@@ -0,0 +1,18 @@
+﻿using HttpTracker.Controller;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System.Reflection;
+
+namespace HttpTracker
+{
+    public class HttpTrackerControllerFeatureProvider : ControllerFeatureProvider
+    {
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            if (typeInfo == typeof(HttpTrackerController))
+            {
+                return true;
+            }
+            return base.IsController(typeInfo);
+        }
+    }
+}
