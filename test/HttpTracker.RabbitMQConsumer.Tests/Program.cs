fe29a6b5afe9562c0b9f4057fb2087e783f23b36
阿星Plus <hackxing@foxmail.com>
2020-08-04 09:21:19 +0800

    🆕 RabbitMQ Test

---
+++ test/HttpTracker.RabbitMQConsumer.Tests/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace HttpTracker.RabbitMQConsumer.Tests
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+            };
+
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
