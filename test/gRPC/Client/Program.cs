30eeed96709cbcbddbcad9e55ef9a29d82cb0396
阿星Plus <hackxing@foxmail.com>
2020-08-06 11:17:30 +0800

    🆕 gRPC Test

---
+++ test/gRPC/Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Hello;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpTracker.gRPCClient.Tests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new HelloService.HelloServiceClient(channel);
+
+            await UnaryCallExample(client);
+
+            await ServerStreamingCallExample(client);
+        }
+
+        private static async Task UnaryCallExample(HelloService.HelloServiceClient client)
+        {
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "哈哈哈" });
+
+            Console.WriteLine("Greeting: " + reply.Message);
+        }
+
+        private static async Task ServerStreamingCallExample(HelloService.HelloServiceClient client)
+        {
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(3.5));
+
+            using var call = client.SayHellos(new HelloRequest { Name = "啦啦啦" }, cancellationToken: cts.Token);
+
+            try
+            {
+                await foreach (var message in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine("Greeting: " + message.Message);
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled.");
+            }
+        }
+    }
+}
---
+++ test/gRPC/Server/Services/GreeterService.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Core;
+using Hello;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Server.Services
+{
+    public class GreeterService : HelloService.HelloServiceBase
+    {
+        private readonly ILogger _logger;
+
+        public GreeterService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GreeterService>();
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Sending hello to {request.Name}");
+
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+
+        public override async Task SayHellos(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                var message = $"How are you {request.Name}? {++i}";
+
+                _logger.LogInformation($"Sending greeting {message}.");
+
+                await responseStream.WriteAsync(new HelloReply { Message = message });
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
